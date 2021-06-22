5cd32fe6f7b86533c3984be811ded13f2e515f5b
Satyabrat Panda <46991649+satyap54@users.noreply.github.com>
2021-06-22 12:45:05 +0530

    Update producer.py

--- Microservices/Events/Events/producer.py
+++ Microservices/Events/Events/producer.py
@@ -3,7 +3,7 @@ import aiormq
 
 
 async def publish(method, event_id):
-    connection = await aiormq.connect('amqps://wdwlltwy:mVmwRY959Xx61jK21X_PfoFr8tcgHIBg@cow.rmq2.cloudamqp.com/wdwlltwy')
+    connection = await aiormq.connect('')
     channel = await connection.channel()
 
     body = bytes(str(event_id), 'utf-8')
